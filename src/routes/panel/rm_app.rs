555775834e645797736f50863842b8178ec51b8c
UnkoR00T <99119433+AlexYTGamePL@users.noreply.github.com>
2025-04-23 07:27:25 +0200

    v0.0.1#008 Added: - Remove App route (rm_app.rs, /panel/app/remove POST)

---
+++ src/routes/panel/rm_app.rs
@@ -0,0 +1,35 @@
+use rocket::http::Status;
+use rocket::post;
+use rocket::serde::json::Json;
+use rocket::serde::{Deserialize, Serialize};
+use surrealdb::sql::{Id, Thing};
+use crate::DB;
+use crate::db::error::error::Error;
+use crate::guards::networking_admin_guard::AdminGuard;
+use crate::services::check_app_existence::check_app_existence;
+
+#[derive(Serialize, Deserialize)]
+struct RemoveAppRequest {
+    app_id: String
+}
+
+#[post("/app/remove", data = "<app>")]
+pub async fn rm_app(app: Json<RemoveAppRequest>, admin: AdminGuard) -> Result<Status, Error> {
+    let app_id = app.app_id.clone();
+    if app_id == "Networking" {
+        return Err(Error::Custom(Status::NotAcceptable));
+    }
+    match check_app_existence(&app_id).await{
+        Ok(_) => {
+            let query = r#"
+                DELETE auths WHERE out == $cur_app;
+                DELETE $cur_app;
+            "#;
+            let query = DB.query(query)
+                .bind(("cur_app", Thing::from(("auth_apps", Id::String(app_id)))))
+                .await?;
+            Ok(Status::Ok)
+        },
+        Err(e) => Err(Error::Custom(Status::BadRequest))
+    }
+}
